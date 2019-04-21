e2f581f5480991a92347ca32db89f2d0afda2119
CasparOne <vvolodin.victor@gmail.com>
2019-04-21 23:55:33 +0300

    Remastered class View with Magic methods
    Signed-off-by: CasparOne <vvolodin.victor@gmail.com>

--- App/View.php
+++ App/View.php
@@ -7,36 +7,108 @@ namespace App;
  * Class View
  * @package App
  */
-class View
+class View implements \Countable, \Iterator
 {
+    use MagicTrait;
+
     /**
      * @var array
      */
     protected $data = [];
 
     /**
-     * @param string $name
-     * @param Model $value
-     * @return $this
-     */
-    public function assign(string $name, array $value)
-    {
-        $this->data[$name] = $value;
-        return $this;
-    }
-
-    /**
      * @param $template
-     * @return boolean
+     * @return bool
      */
-    public function display($template) : bool
+    public function display($template)
     {
         extract($this->data);
         if (is_readable($template)) {
             include $template;
-            return true;
         }
         return false;
     }
 
+    /**
+     * @param $template
+     * @return false|string
+     */
+    public function render($template)
+    {
+        ob_start();
+        $this->display($template);
+        $contents = ob_get_contents();
+        ob_end_clean();
+        return $contents;
+    }
+
+    /**
+     * Count elements of an object
+     * @link https://php.net/manual/en/countable.count.php
+     * @return int The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     * @since 5.1.0
+     */
+    public function count()
+    {
+        return count($this->data);
+    }
+
+    /**
+     * Return the current element
+     * @link https://php.net/manual/en/iterator.current.php
+     * @return mixed Can return any type.
+     * @since 5.0.0
+     */
+    public function current()
+    {
+        return $this->current($this->data);
+    }
+
+    /**
+     * Move forward to next element
+     * @link https://php.net/manual/en/iterator.next.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function next()
+    {
+        next($this->data);
+    }
+
+    /**
+     * Return the key of the current element
+     * @link https://php.net/manual/en/iterator.key.php
+     * @return mixed scalar on success, or null on failure.
+     * @since 5.0.0
+     */
+    public function key()
+    {
+        return key($this->data);
+    }
+
+    /**
+     * Checks if current position is valid
+     * @link https://php.net/manual/en/iterator.valid.php
+     * @return boolean The return value will be casted to boolean and then evaluated.
+     * Returns true on success or false on failure.
+     * @since 5.0.0
+     */
+    public function valid()
+    {
+        return null !== key($this->data);
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * @link https://php.net/manual/en/iterator.rewind.php
+     * @return void Any returned value is ignored.
+     * @since 5.0.0
+     */
+    public function rewind()
+    {
+        reset($this->data);
+    }
 }
