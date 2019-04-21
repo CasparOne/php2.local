e40bfd6afb1884e06f451c5ff95a97cd8c7f89f9
CasparOne <vvolodin.victor@gmail.com>
2019-04-21 23:56:01 +0300

    Re-worked index file
    Signed-off-by: CasparOne <vvolodin.victor@gmail.com>

--- index.php
+++ index.php
@@ -6,5 +6,5 @@ use App\View;
 require __DIR__ . '/autoload.php';
 
 $view = new View();
-$view->assign('articles', Article::findLastRecord(4))
-    ->display(__DIR__ . '/template/index.php');
+$view->articles =  Article::findLastRecord(4);
+echo $view->render(__DIR__ . '/template/index.php');
