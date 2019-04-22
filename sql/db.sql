e71fb9402c1bec3f1f72c71bb25fa34b271d4c77
CasparOne <vvolodin.victor@gmail.com>
2019-04-23 00:35:17 +0300

    Made new table Authors
    Signed-off-by: CasparOne <vvolodin.victor@gmail.com>

--- sql/db.sql
+++ sql/db.sql
@@ -1,14 +1,31 @@
-                         create table test.article
+create table articles
 (
 	id bigint unsigned auto_increment,
-	title varchar(200) null,
+	title varchar(400) null,
 	text text null,
-	author varchar(100) null,
-	created timestamp default CURRENT_TIMESTAMP null,
+	created datetime default CURRENT_TIMESTAMP not null,
+	author_id bigint unsigned null,
+	constraint article_id_uindex
+		unique (id),
 	constraint id
 		unique (id)
 );
 
-alter table test.article
+alter table articles
+	add primary key (id);
+
+create table authors
+(
+	id bigint unsigned auto_increment,
+	first_name varchar(200) null,
+	last_name varchar(200) null,
+	nick_name varchar(200) null,
+	constraint authors_id_uindex
+		unique (id),
+	constraint id
+		unique (id)
+);
+
+alter table authors
 	add primary key (id);
 
